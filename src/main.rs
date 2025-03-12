d634ff95b88248ba98c39a0c0d6e9fce87a71e55
AagabBhattarai <aagabbhattarai124@gmail.com>
2025-03-12 15:31:26 +0545

    BPE Working but some error maybe present

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use std::collections::HashMap;
 use std::fs;
-use std::io;
+use std::io::{self, Write};
 
 fn read_corpus(corpus_path: &str) -> Result<String> {
     let corpus: String = fs::read_to_string(corpus_path)?;
@@ -36,7 +36,9 @@ fn tokenize(word: &str, vocabulary: &HashMap<String, usize>) -> Vec<String> {
 
     let mut sorted_vocab: Vec<(&str, usize)> =
         vocabulary.iter().map(|(k, v)| (k.as_str(), *v)).collect();
-    sorted_vocab.sort_by_key(|(_, v)| *v);
+    sorted_vocab.sort_by(|(k1, v1), (k2, v2)| v1.cmp(v2).then(k2.cmp(k1)));
+    // sorted_vocab.sort_by_key(|(_, v)| *v);
+    println!("SORTED VOCAB {:?}", sorted_vocab);
 
     let mut tokenized_word_vector: Vec<(String, bool)> = vec![(word.to_string(), false)];
     let mut copy_of_working: Vec<(String, bool)> = tokenized_word_vector.clone();
@@ -77,6 +79,42 @@ fn tokenize(word: &str, vocabulary: &HashMap<String, usize>) -> Vec<String> {
     tokenized_word_vector.into_iter().map(|(v, _)| v).collect()
 }
 
+fn build_bpe_vocabulary(
+    corpus: &str,
+    vocabulary: &mut HashMap<String, usize>,
+    // vocab_size: usize,
+    token_frequency: usize,
+) {
+    let tokenized_output = tokenize(corpus, vocabulary);
+    let mut potential_token: HashMap<String, usize> = HashMap::new();
+    tokenized_output.windows(2).for_each(|c| {
+        let new_token = c[0].to_string() + c[1].as_str();
+        potential_token
+            .entry(new_token)
+            .and_modify(|c| *c += 1)
+            .or_insert(1);
+    });
+    let mut potential_token_vec: Vec<(String, usize)> = potential_token.into_iter().collect();
+    // potential_token_vec.sort_by_key(|(_, v)| *v);
+    potential_token_vec.sort_by(|(k1, v1), (k2, v2)| v2.cmp(v1).then(k1.cmp(k2)));
+    // potential_token_vec.sort_by_key(|(w, _)| w.clone());
+
+    println!("MAX FREQ VOCABA{:?}", potential_token_vec);
+    // let (token, len) = potential_token_vec.pop().unwrap();
+    // let mut max_freq_tokens: Vec<(String, usize)> = potential_token_vec
+    //     .into_iter()
+    //     .filter(|(_, v)| *v == len)
+    //     .collect();
+    // max_freq_tokens.push((token.clone(), len));
+    // max_freq_tokens.sort_by_key(|(w, _)| w.clone());
+    // println!("MAX FREQ: {:?}", max_freq_tokens);
+    // let (token, _) = max_freq_tokens.remove(0);
+    let (token, len) = potential_token_vec.remove(0);
+    if len >= token_frequency {
+        vocabulary.insert(token.clone(), token.len());
+    }
+}
+
 fn main() -> Result<()> {
     // let corpus_path = "data/corpus.txt";
     let corpus_path = "data/test.txt";
@@ -101,13 +139,29 @@ fn main() -> Result<()> {
     }
     // word_to_tokenize = corpus.trim().to_string().clone();
     word_to_tokenize = corpus.to_string().clone();
-    vocabulary.insert("AAGAB".to_string(), 5);
+    // vocabulary.insert("AAGAB".to_string(), 5);
     // word_to_tokenize.split_at
 
     let tokenized_output = tokenize(&word_to_tokenize, &vocabulary);
-    let string_output = tokenized_output.join("|");
-    println!("{:?}", tokenized_output);
-    println!("{}", string_output);
+    let string_output = format!("|{}", tokenized_output.join("|"));
+    println!("Output Vector:\n{:?}", tokenized_output);
+    println!("Tokenized Result:\n{}", string_output);
+
+    // while vocabulary.len() <= 10 {
+    build_bpe_vocabulary(&corpus, &mut vocabulary, 2);
+    build_bpe_vocabulary(&corpus, &mut vocabulary, 2);
+    println!("Vocab Length{}", vocabulary.len());
+    // }
+    for (k, v) in vocabulary.iter() {
+        println!("{}: {}", k, v);
+    }
+    let tokenized_output = tokenize(&word_to_tokenize, &vocabulary);
+    println!("Output Vector:\n{:?}", tokenized_output);
+
+    let mut file = fs::File::create("data/vocab.txt").unwrap();
+    for (k, v) in vocabulary.iter() {
+        writeln!(file, "{}: {}", k, v)?;
+    }
 
     // split_with_separators(word_to_tokenize, "AAGAB".to_string());
 
