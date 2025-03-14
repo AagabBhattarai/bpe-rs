0e8b6472d1d5c972cfe4a69f9ec265b4c6c734c7
AagabBhattarai <aagabbhattarai124@gmail.com>
2025-03-14 13:13:38 +0545

    Done

--- src/main.rs
+++ src/main.rs
@@ -9,79 +9,6 @@ fn read_corpus(corpus_path: &str) -> Result<String> {
     Ok(corpus)
 }
 
-#[allow(dead_code)]
-fn split_with_separators_old(s: String, separator: String) -> Vec<String> {
-    let split_vec: Vec<String> = s
-        .split_inclusive(&separator)
-        .map(|e| e.to_string())
-        .collect();
-    let mut fully_split: Vec<String> = Vec::new();
-    for word in split_vec {
-        if word.contains(&separator) {
-            let sep_idx = word.len() - separator.len();
-            if sep_idx != 0 {
-                fully_split.push(word[..sep_idx].to_string());
-            }
-            fully_split.push(word[sep_idx..].to_string());
-        } else {
-            fully_split.push(word);
-        }
-    }
-    fully_split
-}
-fn split_with_separators(s: &str, separator: String) -> Vec<String> {
-    let split_idx: Vec<_> = s.match_indices(&separator).collect();
-    let mut walker = 0;
-    let mut split_value: Vec<String> = Vec::new();
-    for (idx, _v) in split_idx {
-        if walker != idx {
-            split_value.push(s[walker..idx].to_string());
-        }
-        split_value.push(separator.clone());
-        walker = idx + separator.len();
-    }
-    if walker != s.len() {
-        split_value.push(s[walker..].to_string());
-    }
-    split_value
-}
-
-#[allow(unused_assignments)]
-#[allow(dead_code)]
-fn tokenize(word: &str, vocabulary: &HashMap<String, usize>) -> Vec<String> {
-    let mut sorted_vocab: Vec<(&str, usize)> =
-        vocabulary.iter().map(|(k, v)| (k.as_str(), *v)).collect();
-    sorted_vocab.sort_by(|(k1, v1), (k2, v2)| v1.cmp(v2).then(k2.cmp(k1)));
-
-    let mut tokenized_word_vector: Vec<(String, bool)> = vec![(word.to_string(), false)];
-    let mut copy_of_working: Vec<(String, bool)> = tokenized_word_vector.clone();
-
-    let mut temp_vector: Vec<(String, bool)> = Vec::new();
-    for (token, _) in sorted_vocab.into_iter().rev() {
-        let mut offset = 0;
-        for (i, (w, t)) in tokenized_word_vector.iter().enumerate() {
-            if !t {
-                let tokenized_vector = split_with_separators(w, token.to_string());
-                temp_vector = tokenized_vector
-                    .into_iter()
-                    .map(|e| if e == token { (e, true) } else { (e, false) })
-                    .collect();
-                let idx = i + offset;
-                copy_of_working.remove(idx);
-                copy_of_working.splice(idx..idx, temp_vector.clone());
-                offset += temp_vector.len() - 1;
-            }
-        }
-        tokenized_word_vector = copy_of_working.clone();
-    }
-
-    tokenized_word_vector.into_iter().map(|(v, _)| v).collect()
-}
-#[allow(dead_code)]
-fn character_level_tokenize(word: &str) -> Vec<String> {
-    word.chars().map(|c| c.to_string()).collect()
-}
-
 fn merge_tokenized_corpus(tokenized_corpus: &Vec<String>, token: String) -> Vec<String> {
     let mut new_tokenized_corpus: Vec<String> = Vec::with_capacity(tokenized_corpus.len());
     let mut skip = false;
@@ -96,8 +23,30 @@ fn merge_tokenized_corpus(tokenized_corpus: &Vec<String>, token: String) -> Vec<
             new_tokenized_corpus.push(c[0].to_string());
         }
     });
+    if !skip {
+        new_tokenized_corpus.push(tokenized_corpus.last().unwrap().to_string());
+    }
     new_tokenized_corpus
 }
+// fn merge_tokenized_corpus(tokenized_corpus: &[String], token: String) -> Vec<String> {
+//     let mut new_tokenized_corpus = Vec::with_capacity(tokenized_corpus.len());
+//     let mut i = 0;
+
+//     while i < tokenized_corpus.len() {
+//         if i < tokenized_corpus.len() - 1
+//             && tokenized_corpus[i].clone() + &tokenized_corpus[i + 1] == token
+//         {
+//             new_tokenized_corpus.push(token.to_string());
+//             i += 2; // Skip the next token since it was merged
+//         } else {
+//             new_tokenized_corpus.push(tokenized_corpus[i].clone());
+//             i += 1;
+//         }
+//     }
+
+//     new_tokenized_corpus
+// }
+
 fn build_bpe_vocabulary(
     tokenized_corpus: &Vec<String>,
     vocabulary: &mut HashMap<String, usize>,
@@ -147,10 +96,6 @@ fn main() -> Result<()> {
     }
     println!("Corpus Size: {}", corpus.len());
 
-    // word_to_tokenize = corpus[..].to_string();
-    // let mut tokenized_output = tokenize(&word_to_tokenize, &vocabulary);
-    // let mut tokenized_output = character_level_tokenize(&word_to_tokenize);
-
     loop {
         let (len, new_tokenized_output) =
             build_bpe_vocabulary(&tokenized_output, &mut vocabulary, 2);
@@ -165,6 +110,7 @@ fn main() -> Result<()> {
     for (k, v) in vocabulary.iter() {
         println!("{}: {}", k, v);
     }
+
     let string_output = format!("|{}", tokenized_output.join("|"));
     println!("Tokenized Result:\n{}", string_output);
 
